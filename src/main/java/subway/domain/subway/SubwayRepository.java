9ca4f0bb5a0f024f210ebf90d97cadde0c176207
iamsseaa <homingsea@gmail.com>
2021-12-16 19:01:21 +0900

    feat: 모든 호선을 관리하는 SubwayRepository 클래스 생성

--- src/main/java/subway/domain/subway/SubwayRepository.java
+++ src/main/java/subway/domain/subway/SubwayRepository.java
@@ -1,4 +1,14 @@
 package subway.domain.subway;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SubwayRepository {
+    static List<Subway> subways = new ArrayList<>();
+
+    public static List<Subway> subways() {
+        return Collections.unmodifiableList(subways);
+    }
+    
 }
