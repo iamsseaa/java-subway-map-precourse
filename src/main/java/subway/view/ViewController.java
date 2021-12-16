4c137ac55e6806917b6393ac8f55b955075c1375
iamsseaa <homingsea@gmail.com>
2021-12-16 23:18:56 +0900

    feat: 프로그램 시작 전 세팅

--- src/main/java/subway/view/ViewController.java
+++ src/main/java/subway/view/ViewController.java
@@ -1,4 +1,12 @@
 package subway.view;
 
 public class ViewController {
+    public static void setPrepare() {
+        PreSetting.setStations();
+        PreSetting.setLines();
+        PreSetting.setSubways();
+        PreSetting.setSubwayStation();
+    }
+
+    
 }
