90a999c33570bc8f2ed3a86c351bd2ea7d974570
iamsseaa <homingsea@gmail.com>
2021-12-16 18:58:49 +0900

    feat: 한 호선을 관리하는 subway 클래스 생성

--- src/main/java/subway/domain/subway/Subway.java
+++ src/main/java/subway/domain/subway/Subway.java
@@ -1,4 +1,18 @@
 package subway.domain.subway;
 
+import subway.domain.station.Station;
+
+import javax.sound.sampled.Line;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subway {
+    Line line;
+    List<Station> stations = new ArrayList<>();
+
+    public Subway(Line line, Station upStation, Station downStation) {
+        this.line = line;
+        stations.add(upStation);
+        stations.add(downStation);
+    }
 }
