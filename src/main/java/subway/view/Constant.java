c0eea07212cf697e28b27d33fc3489f282f68964
iamsseaa <homingsea@gmail.com>
2021-12-16 23:18:15 +0900

    feat: 메인 화면 문구 상수

--- src/main/java/subway/view/Constant.java
+++ src/main/java/subway/view/Constant.java
@@ -1,4 +1,18 @@
 package subway.view;
 
 public class Constant {
+    public static final String MAIN = "## 메인 화면";
+    public static final String ANSWER_ONE = "1. 역 관리";
+    public static final String ANSWER_TWO = "2. 노선 관리";
+    public static final String ANSWER_THREE = "3. 구간 관리";
+    public static final String ANSWER_FOUR = "4. 지하철 노선도 출력";
+    public static final String ANSWER_QUIT = "Q. 종료";
+
+    public static final String ASK_MAIN_VIEW = "## 원하는 기능을 선택하세요.";
+
+    public static final String CHOICE_STATION_VIEW = "1";
+    public static final String CHOICE_LINE_VIEW = "2";
+    public static final String CHOICE_SUBWAY_VIEW = "3";
+    public static final String CHOICE_PRINT_SUBWAY_VIEW = "4";
+    public static final String CHOICE_QUIT = "Q";
 }
